58bf3c1231f603cfcc3439c37a5cb0951606612e
Ashish Jha <ajha5645@gmail.com>
2024-05-22 08:21:25 +0530

    Update Razorpay configuration to use environment variables

--- utils/razorpay/razorpay.ts
+++ utils/razorpay/razorpay.ts
@@ -1,5 +1,6 @@
 import Razorpay from 'razorpay';
-
+require('dotenv').config();
+console.log(process.env.RAZORPAY_KEY_ID);
 export const instance = new Razorpay({
     key_id: process.env.RAZORPAY_KEY_ID!,
     key_secret: process.env.RAZORPAY_KEY_SECRET!,
